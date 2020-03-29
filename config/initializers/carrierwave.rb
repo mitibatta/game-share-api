de247b5ceb1e277b6676695c63ddfef67d08512a
今岡悠 <mitibatta@my-mba.local>
2020-03-29 12:36:57 +0900

    S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
-    config.asset_host = 'https://game-share-api.herokuapp.com'
+    config.asset_host = 'https://s3.amazonaws.com/gsimage'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: 'AKIATLKQBI5SFVKEAGMI',
       aws_secret_access_key: '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',
       region: 'ap-northeast-1'
     }
-
+    config.fog_public = false 
     config.fog_directory  = 'gsimage'
     config.cache_storage = :fog
   end
